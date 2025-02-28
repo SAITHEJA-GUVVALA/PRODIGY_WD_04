70cf711cd00564fdc7b1d8513a03a74c7e9b890d
SAITHEJA-GUVVALA <saithejaguvvala@gmail.com>
2025-02-28 13:05:11 +0530

    Add files via upload

---
+++ PRODIGY_WD_04/script.js
@@ -0,0 +1,41 @@
+// Navigation Menu
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+// Tab Switching
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Contact Form Submission
+const scriptURL = 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully!";
+            setTimeout(() => msg.innerHTML = "", 1000);
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message));
+});
